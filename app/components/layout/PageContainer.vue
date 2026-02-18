984a12c07436b67d18179d9b48b23be0a637837a
MiggRabbid <mig26@inbox.ru>
2026-02-18 18:07:41 +0300

    Update: PageContainer

---
+++ app/components/layout/PageContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="page-container">
+    <slot />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.page-container {
+  display: flex;
+  min-height: calc(100dvh - 82px);
+  margin: 0 auto;
+  max-width: var(--container-max-width);
+  padding-left: var(--container-padding-desktop);
+  padding-right: var(--container-padding-desktop);
+  padding-top: var(--container-padding-desktop-y);
+  padding-bottom: var(--container-padding-desktop-y);
+}
+
+@media (max-width: 968px) {
+  .page-container {
+    padding-left: var(--container-padding-tablet);
+    padding-right: var(--container-padding-tablet);
+    padding-top: var(--container-padding-tablet);
+    padding-bottom: var(--container-padding-tablet-y);
+  }
+}
+
+@media (max-width: 640px) {
+  .page-container {
+    padding-left: var(--container-padding-mobile);
+    padding-right: var(--container-padding-mobile);
+    padding-top: var(--container-padding-mobile);
+    padding-bottom: var(--container-padding-mobile-y);
+  }
+}
+</style>
